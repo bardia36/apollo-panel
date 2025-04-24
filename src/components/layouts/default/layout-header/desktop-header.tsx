bf4517889b7040c6aa8b1d3d9fbfb6e3f300b807
matin <matin3061379@gmail.com>
2025-04-25 00:49:29 +0330

    feat: Add Breadcrumb

--- src/components/layouts/default/layout-header/desktop-header.tsx
+++ src/components/layouts/default/layout-header/desktop-header.tsx
@@ -1,9 +1,15 @@
 import { FullLogo } from "@/components/shared/logo";
+import Breadcrumb from "./breadcrumb";
+import { t } from "i18next";
 
 export default function DesktopHeader() {
+  const breadcrumbLinks = [
+    { name: t("title.expertRequests"), url: "/expert-requests" },
+  ];
+
   return (
     <div className="flex items-center justify-between w-full h-16 px-4 pt-4">
-      Bread crumb
+      <Breadcrumb links={breadcrumbLinks} />
       <FullLogo classNames={{ img: "h-[2.625rem]" }} />
     </div>
   );
