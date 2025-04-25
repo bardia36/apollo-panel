71a91a480316f6a780a7a3d6ef5967f002944744
matin <matin3061379@gmail.com>
2025-04-26 00:04:33 +0330

    feat: Add type tabs

--- src/services/translate/fa/expert-requests.ts
+++ src/services/translate/fa/expert-requests.ts
@@ -1,8 +1,10 @@
 export default {
+  archive: "بایگانی",
+  createRequest: "ایجاد درخواست",
+  currentCases: "پرونده‌های جاری",
   management: "مدیریت",
   requests: "درخواست‌ها",
   requestsManagement: "مدیریت درخواست‌ها",
   requestsPageDescription: "مشاهده و مدیریت لیست درخواست‌های بازدید آنلاین",
   templates: "تمپلیت‌ها",
-  createRequest: "ایجاد درخواست",
 };
