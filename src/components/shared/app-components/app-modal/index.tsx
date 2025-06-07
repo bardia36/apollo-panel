dbceabf12e83e28206b8c538868d886d232178e6
matinshamsaei <matin3061379@gmail.com>
2025-06-07 13:13:21 +0330

    feat: Add features to the AppModal
    add header and footer slots
    add modalRef

--- src/components/shared/app-components/app-modal/index.tsx
+++ src/components/shared/app-components/app-modal/index.tsx
@@ -1,82 +1,150 @@
 import {
   cloneElement,
-  FC,
   isValidElement,
   MouseEvent,
   ReactNode,
   useCallback,
+  useImperativeHandle,
+  forwardRef,
+  ForwardRefExoticComponent,
+  RefAttributes,
 } from "react";
 import {
   Modal,
   ModalContent,
-  ModalBody,
   useDisclosure,
   ModalProps,
   cn,
+  ModalHeader,
+  ModalFooter,
 } from "@heroui/react";
 import { useBreakpoint } from "@/hooks/useBreakpoint";
 
-type Props = {
-  activator: ReactNode;
+export type AppModalRef = {
+  onClose: () => void;
+  onOpen: () => void;
+  isOpen: boolean;
+};
+
+type AppModalHeaderProps = {
+  children: ReactNode;
+  className?: string;
+};
+
+const AppModalHeader = ({ children, className }: AppModalHeaderProps) => {
+  return (
+    <ModalHeader className={cn("flex items-center gap-2 p-0", className)}>
+      {children}
+    </ModalHeader>
+  );
+};
+
+type AppModalFooterProps = {
+  children: ReactNode;
+  className?: string;
+};
+
+const AppModalFooter = ({ children, className }: AppModalFooterProps) => {
+  return (
+    <ModalFooter className={cn("pt-4 px-0 pb-0", className)}>
+      {children}
+    </ModalFooter>
+  );
+};
+
+type AppModalProps = {
+  activator?: ReactNode;
   children: ReactNode;
   size?: ModalProps["size"];
   className?: string;
+  classNames?: ModalProps["classNames"];
   isDismissable?: boolean;
   hideCloseButton?: boolean;
 };
 
-export const AppModal: FC<Props> = ({
-  activator,
-  children,
-  size,
-  className,
-  isDismissable,
-  hideCloseButton,
-}) => {
-  const { isOpen, onOpen, onClose } = useDisclosure();
-  const { isMdAndUp } = useBreakpoint();
+interface AppModalComponent
+  extends ForwardRefExoticComponent<
+    AppModalProps & RefAttributes<AppModalRef>
+  > {
+  Header: typeof AppModalHeader;
+  Footer: typeof AppModalFooter;
+}
 
-  const handleModalClose = useCallback(() => {
-    onClose();
-  }, [onClose]);
+const AppModalBase = forwardRef<AppModalRef, AppModalProps>(
+  (
+    {
+      activator,
+      children,
+      size,
+      className,
+      classNames,
+      isDismissable,
+      hideCloseButton,
+    },
+    ref
+  ) => {
+    const { isOpen, onOpen, onClose } = useDisclosure();
+    const { isMdAndUp } = useBreakpoint();
 
-  const handleActivatorClick = (e: MouseEvent<HTMLElement>) => {
-    if (isValidElement(activator) && activator.props.onClick) {
-      activator.props.onClick(e);
-    }
-    onOpen();
-  };
+    useImperativeHandle(ref, () => ({
+      isOpen,
+      onOpen,
+      onClose,
+    }));
 
-  const activatorWithProps = isValidElement(activator) ? (
-    cloneElement(activator, {
-      ...(typeof activator.type === "string" &&
-      activator.type.toLowerCase() === "button"
-        ? { onPress: handleActivatorClick }
-        : { onClick: handleActivatorClick }),
-    })
-  ) : (
-    <span onClick={onOpen}>{activator}</span>
-  );
+    const handleModalOpen = useCallback(() => {
+      onOpen();
+    }, [onOpen]);
 
-  return (
-    <>
-      {activatorWithProps}
+    const handleModalClose = useCallback(() => {
+      onClose();
+    }, [onClose]);
 
-      {isOpen && (
-        <Modal
-          isOpen={isOpen}
-          backdrop="blur"
-          hideCloseButton={(hideCloseButton ?? isMdAndUp) ? true : false}
-          isDismissable={isDismissable ?? false}
-          className={cn(className, "my-auto")}
-          size={isMdAndUp ? size : "full"}
-          onClose={handleModalClose}
-        >
-          <ModalContent>
-            <ModalBody className="p-4 overflow-y-auto">{children}</ModalBody>
-          </ModalContent>
-        </Modal>
-      )}
-    </>
-  );
-};
+    const handleActivatorClick = (e: MouseEvent<HTMLElement>) => {
+      if (isValidElement(activator) && activator.props.onClick)
+        activator.props.onClick(e);
+      handleModalOpen();
+    };
+
+    const activatorWithProps = activator ? (
+      isValidElement(activator) ? (
+        cloneElement(activator, {
+          ...(typeof activator.type === "string" &&
+          activator.type.toLowerCase() === "button"
+            ? { onPress: handleActivatorClick }
+            : { onClick: handleActivatorClick }),
+        })
+      ) : (
+        <span onClick={handleModalOpen}>{activator}</span>
+      )
+    ) : null;
+
+    return (
+      <>
+        {activatorWithProps}
+
+        {isOpen && (
+          <Modal
+            isOpen={isOpen}
+            backdrop="blur"
+            hideCloseButton={(hideCloseButton ?? isMdAndUp) ? true : false}
+            isDismissable={isDismissable ?? false}
+            classNames={{
+              ...classNames,
+              closeButton: `${classNames?.closeButton} ${"top-4 md:top-6 left-4 md:left-6"}`,
+            }}
+            className={cn(className, "my-auto")}
+            size={isMdAndUp ? size : "full"}
+            onClose={handleModalClose}
+          >
+            <ModalContent className="gap-6 p-4 md:p-6">{children}</ModalContent>
+          </Modal>
+        )}
+      </>
+    );
+  }
+);
+
+export const AppModal = AppModalBase as AppModalComponent;
+AppModal.Header = AppModalHeader;
+AppModal.Footer = AppModalFooter;
